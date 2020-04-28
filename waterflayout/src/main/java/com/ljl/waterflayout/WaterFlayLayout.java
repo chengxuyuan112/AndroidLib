f2079680de118d2537e9af721b40e870b0ce6486
lzhang <lzhang@haimo.com.cn>
2020-04-28 18:31:36 +0800

    測試1

--- waterflayout/src/main/java/com/ljl/waterflayout/WaterFlayLayout.java
+++ waterflayout/src/main/java/com/ljl/waterflayout/WaterFlayLayout.java
@@ -5,6 +5,16 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: ljl
+ * @Time: 2020/4/28 16:56
+ * @Company：Haimo
+ * @Description: 功能描述
+ */
 public class WaterFlayLayout extends ViewGroup {
     public WaterFlayLayout(Context context) {
         super(context);
@@ -24,66 +34,72 @@ public class WaterFlayLayout extends ViewGroup {
 
     @Override
     public LayoutParams generateLayoutParams(AttributeSet attrs) {
-        return new MarginLayoutParams(getContext(),attrs);
+        return new MarginLayoutParams(getContext(), attrs);
     }
 
     @Override
-    public LayoutParams getLayoutParams() {
-        return super.getLayoutParams();
+    protected LayoutParams generateLayoutParams(LayoutParams p) {
+        return new MarginLayoutParams(p);
     }
+
+    private List<View> linView = new ArrayList<>();
+    private List<List<View>> allLinList = new ArrayList<>();
+    List<Integer> lstLineHegiht = new ArrayList<>();
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-         // 1 先完成自己的宽高测试
-         // 需要得到mode进行判断我的显示模式是什么？
+        //获取父类的大小
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+        //获取自身的mode
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        //测量的宽高，最后设置的宽高
+        int measureWidth = 0;
+        int measureHeight = 0;
 
-        //得到父类宽高
-       int widthSize= MeasureSpec.getSize(widthMeasureSpec);
-       int heightSize=MeasureSpec.getSize(heightMeasureSpec);
+        //当前行宽高
+        int currentWidth = 0;
+        int currentHeight = 0;
 
-       //得到自己的测量模式
-        int widthMode=MeasureSpec.getMode(widthMeasureSpec);
-        int heightMode=MeasureSpec.getMode(heightMeasureSpec);
+        //各个child测量的宽高
+        int childWidth = 0;
+        int childHeight = 0;
+        if (widthMode == MeasureSpec.EXACTLY && heightMode == MeasureSpec.EXACTLY) { //设置同父类一样大小
+            measureWidth = widthSize;
+            measureHeight = heightSize;
+        } else {
+            //获取所有的child
+            int childCount = getChildCount();
+            for (int i = 0; i < childCount; i++) {
+                //获取每个子view
+                View child = getChildAt(i);
+                //子view 自己测量
+                measureChild(child, widthSize, heightSize);
 
-        //当前控件自己的宽高
-        int measureWidth=0;
-        int measureHeight=0;
-        //layout_width=match_parent layout_height=match_parent
-          if (widthMode==MeasureSpec.EXACTLY&&heightMode==MeasureSpec.EXACTLY){
-              measureWidth=widthSize;
-              measureHeight=heightSize;
-          }else {
-             // 获取所有的child 的总和
-              int childCount = getChildCount();
-              //当前行的高宽
-              int iChildWidth=0;
-              int iChildHeight=0;
+                //获取MARGIN 获取到当前LayoutParams
+                MarginLayoutParams layoutParams = (MarginLayoutParams) child.getLayoutParams();
+                childWidth = child.getMeasuredWidth() + layoutParams.leftMargin + layoutParams.rightMargin;
+                childHeight = child.getMeasuredHeight() + layoutParams.topMargin + layoutParams.bottomMargin;
 
-              //当前行的宽高
-              int CurrWidth=0;
-              int CurrHeight=0;
-              for (int i = 0; i < childCount; i++) {
-                  //获取每一个child
-                     View child =getChildAt(i);
-                     //获取当前child 的了LayoutParams
-                  MarginLayoutParams layoutParams = (MarginLayoutParams)child.getLayoutParams();
-                     //child 先自己测量自己
-                     measureChild(child,widthMeasureSpec,heightMeasureSpec);
-                     iChildWidth =child.getMeasuredWidth()+layoutParams.leftMargin+layoutParams.rightMargin;
-                     iChildHeight=child.getMeasuredHeight()+layoutParams.topMargin+layoutParams.bottomMargin;
-                     if (iChildWidth+CurrWidth>widthSize){ //换行
-                          //保存当前行信息
-                         measureWidth =Math.max(measureWidth,CurrWidth);
-                         measureHeight+=CurrHeight;
-                          CurrWidth =iChildWidth;
-                          CurrHeight= iChildHeight;
-                     }else {
-                           CurrWidth+=iChildWidth;
-                           CurrHeight=Math.max(CurrHeight,iChildHeight);
-                     }
-              }
-          }
-          setMeasuredDimension(measureWidth,measureHeight);
+                if (currentWidth + childWidth > widthSize) { //换行
+                    measureWidth = Math.max(measureWidth, currentWidth);
+                    measureHeight = currentHeight + childHeight;
+                    allLinList.add(linView);
+                    linView = new ArrayList<>();
+                    linView.add(child);
+                    currentWidth=childWidth;
+                    currentHeight=childHeight;
+
+                } else {
+                    currentWidth += childWidth;
+                    currentHeight = Math.max(currentHeight, childHeight);
+                    linView.add(child);
+                }
+            }
+        }
+        setMeasuredDimension(measureWidth, measureHeight);
+
     }
 
     @Override
